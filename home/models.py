b23c2c7bf978b5ef28bb42840223768cd2c24fe9
Christopher Templado <systemplado@users.noreply.github.com>
2021-07-30 10:16:34 +0800

    added blog index page, updated blog page and base html (#1)
    - blog index page
      - added fetching posts by last_published_at
      - added parent/subpage types to limit parent/child pages
    - base html
      - updated base.html with nav and footer
      - redirected home page to blog index page
      - added font awesome 5
    - blog page
      - added conditional for section block title
      - added parent/subpage types to limit parent/child pages

--- home/models.py
+++ home/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from django.http.response import HttpResponseRedirect
 
 
 class HomePage(Page):
     max_count = 1
+
+    parent_page_types = []
+    subpage_types = ['blog.BlogIndexPage']
+
+    def serve(self, request):
+        return HttpResponseRedirect('/blog-index')

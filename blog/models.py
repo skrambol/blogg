f3289fb38a8ab3309862432d8676488f05edb599
Christopher Templado <systemplado@users.noreply.github.com>
2021-07-30 14:49:39 +0800

    added tags and pagination support (#2)
    * added tags
    - updated blog page model to have tags
    - updated blog (index) page template to follow tags
    - updated image block css to make responsive images
    - added media directory to .gitignore
    - untracked media directory
    
    * added pagination for blog index page
    - fixed django debug toolkit
    
    * used static css file
    - moved "clear tags" beside the tag information on blog index page
    - reverted font-color of blog title in blog details (blog index)

--- blog/models.py
+++ blog/models.py
@@ -1,18 +1,43 @@
 from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import StreamField
-from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
+from taggit.models import TaggedItemBase
+from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from streams import blocks
 
-class BlogIndexPage(Page):
+class BlogPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'BlogPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
 
+
+class BlogIndexPage(Page):
     parent_page_types = ['home.HomePage']
     subpage_types = ['blog.BlogPage']
 
     def get_context(self, request, *args, **kwargs):
         context = super().get_context(request, *args, **kwargs)
         posts = BlogPage.objects.live().public().order_by('-last_published_at')
+        tags = request.GET.get('tag')
+
+        if tags:
+            posts = posts.filter(tags__slug__in=[tags])
+
+        paginator = Paginator(posts, 3)
+        page = request.GET.get('page')
+
+        try:
+            posts = paginator.page(page)
+        except PageNotAnInteger:
+            posts = paginator.page(1)
+        except EmptyPage:
+            posts = paginator.page(paginator.num_pages)
 
         context['posts'] = posts
 
@@ -28,8 +53,10 @@ class BlogPage(Page):
             'section': {'min_num': 1}
         }
     )
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
 
     content_panels = Page.content_panels + [
+        FieldPanel('tags'),
         StreamFieldPanel('content')
     ]
 
--- blogg/urls.py
+++ blogg/urls.py
@@ -32,8 +32,8 @@ urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
-    path("", include(wagtail_urls)),
     path('__debug__/', include(debug_toolbar.urls)),
+    path("", include(wagtail_urls)),
 
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
